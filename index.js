71041a3cbbf131b8bd65dcdccfc813439ea734f0
Rohit <rohhit.rz@gmail.com>
2024-11-18 01:34:12 +0530

    commit

---
+++ index.js
@@ -0,0 +1,28 @@
+//example for global execution stack
+
+// var n=2;
+
+// function square(num){
+//     return num*num;
+// }
+
+// var sq2=square(2);
+// var sq4=square(4);
+
+// console.log(sq2);
+// console.log(sq4);
+
+
+// let x=6;
+// function fn(){
+//     console.log("hellloo");
+// }
+
+// fn();
+// console.log(x);
+
+//Hoisting
+
+let x;
+console.log(x);
+x=7;
