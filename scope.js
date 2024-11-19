f3c6f68a369469b8ecc3c2b4a8c542cb4800a89d
Rohit <rohhit.rz@gmail.com>
2024-11-19 23:13:43 +0530

    commit

--- scope.js
+++ scope.js
@@ -1,9 +1,33 @@
-function a() {
-    c();
-    function c() {
+// function a() {
+//     c();
+//     function c() {
+//     console.log(b);
+//   }
+// }
+
+// var b = 10;
+// a();
+
+
+// {
+//     //compound statements
+//     var a=10;
+//     console.log(a);
+// }
+
+
+//block scope- whatever variable and functions we can access inside a block
+
+{
+    var a=10;
+    let b=20;
+    const c=30;
+    console.log(a);
     console.log(b);
-  }
+    console.log(c);
+    
 }
 
-var b = 10;
-a();
+    console.log(a);
+    console.log(b); //reference error
+    console.log(c); //reference error
