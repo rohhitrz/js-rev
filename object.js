f6e735bc50324abf5831140cc14802b40d57b13b
Rohit <rohhit.rz@gmail.com>
2024-11-23 12:36:17 +0530

    commit

---
+++ object.js
@@ -0,0 +1,16 @@
+function  multiplyBy5(num){
+    return num*5;
+}
+
+multiplyBy5.power=2;
+console.log(multiplyBy5(5));
+console.log(multiplyBy5.power);
+console.log(multiplyBy5.prototype);
+
+
+function createUser(username, score){
+     
+
+}
+
+
