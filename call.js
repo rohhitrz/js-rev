40ca02501c538a41f84b53d3b6746aa39fbf47fb
Rohit <rohhit.rz@gmail.com>
2024-11-27 01:27:47 +0530

    commit

---
+++ call.js
@@ -0,0 +1,16 @@
+function setUsername(username){
+    this.username=username;
+
+}
+
+function createUser(username,email,password){
+    // this.username=username;
+    // setUsername(username);       // this line is not working so for this will use .call() method
+   setUsername.call(this,username);
+    this.email=email;
+    this.password=password;
+
+}
+
+const user=new createUser("chai", "xyz@emaple.com", "123456789");
+console.log((user));
