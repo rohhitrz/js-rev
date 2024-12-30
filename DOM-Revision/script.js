408a71615133cc32417892a668734e31d80fbe0d
Rohit <rohhit.rz@gmail.com>
2024-12-30 23:35:38 +0530

    commit

--- DOM-Revision/script.js
+++ DOM-Revision/script.js
@@ -1,42 +1,29 @@
-const todoInput= document.getElementById('todo-input');
-const addTodoButton= document.getElementById('add-todo-button');
-const todoContainer= document.getElementById('todo-container');
+const todoInput = document.getElementById("todo-input");
+const addTodoButton = document.getElementById("add-todo-button");
+const todoContainer = document.getElementById("todo-container");
 
+addTodoButton.addEventListener("click", () => {
+  let task = todoInput.value.trim();
+  console.log(task);
+  if (task) {
+    const todoItem = document.createElement("h1");
+    todoItem.className = "todo-item";
 
+    const taskText = document.createTextNode(task);
 
-
-addTodoButton.addEventListener("click", ()=>{
-   let task=todoInput.value.trim();
-   console.log(task);
-if(task){
-    const todoItem= document.createElement("h1");
-    todoItem.className= 'todo-item';
-
-    const taskText=document.createTextNode(task)
-  
-    const deleteButton= document.createElement("button");
-    deleteButton.textContent="Delete";
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
 
     todoItem.appendChild(taskText);
     todoItem.appendChild(deleteButton);
     todoContainer.appendChild(todoItem);
-    todoInput.value = '';
+    todoInput.value = "";
 
-    deleteButton.addEventListener('click',(e)=>{
-       todoContainer.removeChild(todoItem);
-        console.log(" deleted ", todoItem.innerText)
-
-    })
-
-
-
-    
-
-    
-}else{
-    alert("Please Enter A task:  ")
-} 
-
-
-
-}) 
+    deleteButton.addEventListener("click", (e) => {
+      todoContainer.removeChild(todoItem);
+      console.log(" deleted ", todoItem.innerText);
+    });
+  } else {
+    alert("Please Enter A task:  ");
+  }
+});
