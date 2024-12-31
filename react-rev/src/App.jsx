783c9518a1206ea8cf7eea09a732e6c42514389f
Rohit <rohhit.rz@gmail.com>
2024-12-31 19:39:58 +0530

    commit

--- react-rev/src/App.jsx
+++ react-rev/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
+import Debouncing from './components/Debouncing'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     
+    <> 
+    <Debouncing/>
     </>
   )
 }
---
+++ react-rev/src/components/Debouncing.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+
+export default function Debouncing() {
+  const [userInput, setUserInput] = useState("");
+  const [debouncedValue, setDebouncedValue] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(userInput);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [userInput]);
+
+  return (
+    <div>
+      <h2>Debounce Example</h2>
+      <input
+        type="text"
+        placeholder="type something.."
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
+      />
+
+      <div>
+        <strong>Input Value:</strong> {userInput};
+      </div>
+      <div>
+        <strong>Debounced Value:</strong> {debouncedValue}
+      </div>
+    </div>
+  );
+}
