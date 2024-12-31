c9885229502c9c79d477ca292eb4bb7c7e2e7d0c
Rohit <rohhit.rz@gmail.com>
2025-01-01 02:10:05 +0530

    commit

--- react-rev/src/App.jsx
+++ react-rev/src/App.jsx
@@ -1,20 +1,33 @@
 import { useState } from 'react'
-import Debouncing from './components/Debouncing'
-import SearchComponent from './LifeCycleMethods/SearchComponent'
-import {UseRefHook} from './Hooks/UseRef/UseRefHook'
-import UseRefPreviousState from './Hooks/UseRef/UseRefPreviousState'
 import './App.css'
+import { UseFetch } from './CustomHooks/UseFetch'
+// import Debouncing from './components/Debouncing'
+// import SearchComponent from './LifeCycleMethods/SearchComponent'
+// import {UseRefHook} from './Hooks/UseRef/UseRefHook'
+// import UseRefPreviousState from './Hooks/UseRef/UseRefPreviousState'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const {data,loading,error}= UseFetch('https://jsonplaceholder.typicode.com/posts');
+
+  if(loading) return <p>loading...</p>
+  if (error) return <p> Error: {error} </p>
 
   return (
     <> 
     {/* <Debouncing/> */}
     {/* <SearchComponent/> */}
     {/* <UseRefHook/> */}
-    <UseRefPreviousState/>
+    {/* <UseRefPreviousState/> */}
+
+    <div>
+      <h1>Posts</h1>
+      <ul>{data.map((post)=>(
+       <li key={post.id}>{post.title}</li>
+
+      ))}</ul>
+    </div>
     
     
 
---
+++ react-rev/src/CustomHooks/UseFetch.jsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+export const UseFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Network response was not okay");
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return {data,loading,error};
+};
