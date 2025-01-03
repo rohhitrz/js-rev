260c07b57b352574f368a7c19abe02983b47279a
Rohit <rohhit.rz@gmail.com>
2025-01-04 01:13:06 +0530

    random

--- Rest-and-Spread-Operators/script.js
+++ Rest-and-Spread-Operators/script.js
@@ -1,37 +1,30 @@
-const arr=[1,2,3,4,5,6,7,8,9];
-
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 //spread=(...)
 
 const arr2 = [...arr];
-console.log(arr)
-console.log(arr2)
+console.log(arr);
+console.log(arr2);
 
+let student = {
+  name: "alpha",
+  age: 68,
+  class: 10,
+};
 
-let student={
-    name: "alpha",
-    age: 68,
-    class: 10
-    
-}
-
-
-let stu={...student};
+let stu = { ...student };
 console.log(stu.name);
-stu["name"]="beta";
+stu["name"] = "beta";
 
 console.log(stu.name);
 console.log(student.name);
 
-
 //rest opearator
 
-function sum(first,second,...rest){
-    console.log(first);
-    console.log(second);
-    console.log(rest);
-    
+function sum(first, second, ...rest) {
+  console.log(first);
+  console.log(second);
+  console.log(rest);
 }
 
-sum(1,2,3,4,5,6,7,8,9,10)
-
+sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
